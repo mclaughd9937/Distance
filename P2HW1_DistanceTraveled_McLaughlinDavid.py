4907be3c6f8ffdeacf0e08f798e6b2f2e1c15bc9
mclaughd9937 <36523859+mclaughd9937@users.noreply.github.com>
2018-02-19 14:49:53 -0500

    Add files via upload

---
+++ P2HW1_DistanceTraveled_McLaughlinDavid.py
@@ -0,0 +1,27 @@
+# CTI 110
+# P2HW1- Distance Traveled
+# David McLaughlin
+# 2/15/2018
+
+# Calculate distance travel
+
+speed = 70
+
+print(" A car is traveling at 70 miles per hour" )
+
+time = 6
+distance = speed * time
+# Distance travel in 6 hours
+
+print("The travel time in 6 hours", distance)
+
+time = 10
+distance = speed * time
+
+print("The travel time in 10 hours", distance)
+
+time = 15
+distance = speed * time
+
+print("The travel time in 15 hours", distance)
+
